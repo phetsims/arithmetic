0c931b3b293fceb697a739f1408bced4430b2b5f
MLearner05 <andrey.zelenkov@mlearner.com>
2014-05-28 15:10:22 +0400

    added multiplication table behaviour for 'divide' screen

--- js/divide/view/MultiplicationTableDivideNode.js
+++ js/divide/view/MultiplicationTableDivideNode.js
@@ -14,7 +14,22 @@ define( function( require ) {
   var MultiplicationTableNode = require( 'ARITHMETIC/common/view/table/MultiplicationTableNode' );
 
   function MultiplicationTableDivideNode( gameModel, levelProperty, levels ) {
+    var self = this;
     MultiplicationTableNode.call( this, levelProperty, levels );
+
+    // set left multiplier selected
+    gameModel.property( 'multiplierLeft' ).lazyLink( function() {
+      if ( self.cells[levelProperty.value - 1] ) {
+        self.cells[levelProperty.value - 1][gameModel.multiplierLeft][0].select();
+      }
+    } );
+
+    // set right multiplier selected
+    gameModel.property( 'multiplierRight' ).lazyLink( function() {
+      if ( self.cells[levelProperty.value - 1] ) {
+        self.cells[levelProperty.value - 1][0][gameModel.multiplierRight].select();
+      }
+    } );
   }
 
   return inherit( MultiplicationTableNode, MultiplicationTableDivideNode );
