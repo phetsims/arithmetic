03c56b0ed32c933a5dedaf001ffd24ea869caaf1
MLearner05 <andrey.zelenkov@mlearner.com>
2014-06-05 19:46:25 +0400

    multiplication table selection multiplier fixes

--- js/multiply/view/MultiplicationTableMultiplyNode.js
+++ js/multiply/view/MultiplicationTableMultiplyNode.js
@@ -13,32 +13,37 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var MultiplicationTableNode = require( 'ARITHMETIC/common/view/table/MultiplicationTableNode' );
 
+  // constants
+  var GAME_STATE = require( 'ARITHMETIC/common/enum/GameState' );
+
   function MultiplicationTableMultiplyNode( gameModel, levelProperty, levels ) {
     var self = this;
     MultiplicationTableNode.call( this, levelProperty, levels, gameModel );
 
-    // set view for multiplication table after choosing left and right multipliers
-    gameModel.property( 'multiplierRight' )
-      .and( gameModel.property( 'multiplierLeft' ) )
-      .lazyLink( function() {
-        if ( self.cells[levelProperty.value - 1] && gameModel.multiplierRight && gameModel.multiplierLeft ) {
+    gameModel.property( 'state' ).lazyLink( function( state ) {
+      // set view for multiplication table after choosing left and right multipliers
+      if ( state === GAME_STATE.NEXT_TASK ) {
 
-          // set select multipliers
-          self.cells[levelProperty.value - 1][0][gameModel.multiplierRight].select();
-          self.cells[levelProperty.value - 1][gameModel.multiplierLeft][0].select();
+        // set select multipliers
+        self.cells[levelProperty.value - 1][0][gameModel.multiplierRight].select();
+        self.cells[levelProperty.value - 1][gameModel.multiplierLeft][0].select();
 
-          // set select products
-          self.cells[levelProperty.value - 1].forEach( function( multiplierLeft, index ) {
-            if ( index && index <= gameModel.multiplierLeft ) {
-              multiplierLeft.forEach( function( cell, index ) {
-                if ( index && index <= gameModel.multiplierRight ) {
-                  cell.select();
-                }
-              } );
-            }
-          } );
-        }
-      } );
+        // set select products
+        self.cells[levelProperty.value - 1].forEach( function( multiplierLeft, index ) {
+          if ( index && index <= gameModel.multiplierLeft ) {
+            multiplierLeft.forEach( function( cell, index ) {
+              if ( index && index <= gameModel.multiplierRight ) {
+                cell.select();
+              }
+            } );
+          }
+        } );
+      }
+      // clear cells before game state
+      else if ( state === GAME_STATE.START ) {
+        self.clearCells( levelProperty.value );
+      }
+    } );
   }
 
   return inherit( MultiplicationTableNode, MultiplicationTableMultiplyNode );
