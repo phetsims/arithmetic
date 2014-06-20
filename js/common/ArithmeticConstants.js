827e19d08ad0cd724839e76685118c6871f2cc55
MLearner05 <andrey.zelenkov@mlearner.com>
2014-06-20 21:29:22 +0400

    #13 fixed color difference

--- js/common/ArithmeticConstants.js
+++ js/common/ArithmeticConstants.js
@@ -13,10 +13,10 @@ define( function( require ) {
   var Dimension2 = require( 'DOT/Dimension2' );
 
   return {
-    BACKGROUND_COLOR: '#95bcff',
+    BACKGROUND_COLOR: 'rgb(255,245,237)',
     // constants for back button
     BACK_BUTTON: {
-      BASE_COLOR: '#ffcc43', // base color of back button
+      BASE_COLOR: 'rgb(253,140,37)', // base color of back button
       CORNER_RADIUS: 7, // corner radius of back button
       STROKE: 'none', // stroke of back button
       MARGIN: new Dimension2( 20, 10 ) // margin of background of back button
@@ -24,20 +24,20 @@ define( function( require ) {
     // constants for calculator
     CALCULATOR: {
       ARROW_SIZE: 28, // arrow size of enter and backspace
-      BASE_COLOR: '#ADEFFF', // base color for all buttons
+      BASE_COLOR: 'white', // disable base color for all buttons
       BUTTON_SIZE: new Dimension2( 40, 40 ), // size of buttons
-      DISABLE_BASE_COLOR: 'rgb(234,255,43)', // disable base color for all buttons
+      DISABLE_BASE_COLOR: 'rgb(238,253,77)', // disable base color for all buttons
       SPACING: 10 // spacing between buttons
     },
     // constants for control panel
     CONTROL_PANEL: {
       BACKGROUND: {
-        COLOR: '#95bcff', // color of control panel background
+        COLOR: 'rgb(133,173,251)', // color of control panel background
         MARGIN: new Dimension2( 20, 40 ) // size of margin from content
       },
       HSPASING: 5, // spacing between text and values in bottom of control panel
       REFRESH_BUTTON: {
-        BASE_COLOR: '#ffcc43', // base color of back button
+        BASE_COLOR: 'rgb(253,140,37)', // base color of back button
         CORNER_RADIUS: 7, // corner radius of back button
         STROKE: 'none', // stroke of back button
         MARGIN: new Dimension2( 20, 10 ) // margin of background of back button
@@ -56,21 +56,21 @@ define( function( require ) {
     // constants for multiplication table
     MULTIPLICATION_TABLE: {
       MULTIPLIER_BUTTON: {
-        NORMAL_COLOR_BACKGROUND: 'rgb(239,0,15)', // multiplier button background normal color
-        SELECT_COLOR_BACKGROUND: 'rgb(3,0,205)', // multiplier button background select color
+        NORMAL_COLOR_BACKGROUND: 'rgb(242,38,33)', // multiplier button background normal color
+        SELECT_COLOR_BACKGROUND: 'rgb(14,51,211)', // multiplier button background select color
         NORMAL_COLOR_TEXT: 'white',  // multiplier button text normal color
-        SELECT_COLOR_TEXT: 'yellow'  // multiplier button text select color
+        SELECT_COLOR_TEXT: 'rgb(255,253,56)'  // multiplier button text select color
       },
       PRODUCT_BUTTON: {
-        ACTIVE_COLOR: 'rgb(0,0,119)', // product button active color
-        HOVER_COLOR: 'rgb(234,255,43)', // product button hover color
-        NORMAL_COLOR: 'rgb(3,0,255)', // product button normal color
-        SELECT_COLOR: 'rgb(14,12,70)' // product button select color
+        ACTIVE_COLOR: 'rgb(3,18,136)', // product button active color
+        HOVER_COLOR: 'rgb(238,253,77)', // product button hover color
+        NORMAL_COLOR: 'rgb(14,51,211)', // product button normal color
+        SELECT_COLOR: 'rgb(3,18,136)' // product button select color
       },
       SIZE: new Dimension2( 434, 320 ) // table size
     },
     SCREEN_ICON_SIZE: new Dimension2( 60, 40 ), // size of screen icons
     STAR_NUMBER: 5, // number of stars in select level buttons
-    WORKSPACE_BACKGROUND_COLOR: 'rgb(16,88,253)'
+    WORKSPACE_BACKGROUND_COLOR: 'rgb(28,119,250)'
   };
 } );
