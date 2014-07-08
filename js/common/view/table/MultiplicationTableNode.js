1859663455ed092fcefa7ded6e50cb2d6b68d761
MLearner05 <andrey.zelenkov@mlearner.com>
2014-07-08 17:28:23 +0400

    fixed bug with cells selecting

--- js/common/view/table/MultiplicationTableNode.js
+++ js/common/view/table/MultiplicationTableNode.js
@@ -122,7 +122,6 @@ define( function( require ) {
       // show current multiplication table view for level
       if ( self._viewForLevel[levelNumberCurrent] ) {
         self._viewForLevel[levelNumberCurrent].visible = true;
-        self.clearCells( levelNumberCurrent );
       }
 
       // hide previous multiplication table view
