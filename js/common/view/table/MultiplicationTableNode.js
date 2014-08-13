phetsims/arithmetic

commit bca7313e7e063679220cc629117d553fb09e41d9

Changed back to the /u00D7 Unicode character for multiplication, adjusted the size to what looked good to my eye and best matched the symbol shown in the equation.  See #16.

js/common/view/table/MultiplicationTableNode.js
@@ -77,10 +77,8 @@ define( function( require ) {
           for ( j = 0; j <= tableSize; j++ ) {
             // first cell is 'X', other - multiplier numbers
             if ( j === 0 ) {
-              hBox.addChild( self.cells[levelIndex][i][j] = new MultiplicationTableButtonMultiplierNode( '\u2715', buttonOptions, {
-                font: new PhetFont( {
-                  size: Math.round( 3 * buttonOptions.height / 5 + 2 ),
-                  weight: 'bold'} )
+              hBox.addChild( self.cells[levelIndex][i][j] = new MultiplicationTableButtonMultiplierNode( '\u00D7', buttonOptions, {
+                font: new PhetFont( { size: 40 } ) // Size empirically determined
               } ) );
             }
             else {

commit 6c012678aef8d9c051d3b332cfd8be6ccd3e49cf

Fixed an issue that I introduced with commit https://github.com/phetsims/arithmetic/commit/bca7313e7e063679220cc629117d553fb09e41d9 where the multiply symbol wasn't getting scaled well.  Tried a new scaling equation that seems to be working well.  See #16.

js/common/view/table/MultiplicationTableNode.js
@@ -77,8 +77,9 @@ define( function( require ) {
           for ( j = 0; j <= tableSize; j++ ) {
             // first cell is 'X', other - multiplier numbers
             if ( j === 0 ) {
+              console.log( 'Math.round( buttonOptions.height * 0.85 ) = ' + Math.round( buttonOptions.height * 0.85 ) );
               hBox.addChild( self.cells[levelIndex][i][j] = new MultiplicationTableButtonMultiplierNode( '\u00D7', buttonOptions, {
-                font: new PhetFont( { size: 40 } ) // Size empirically determined
+                font: new PhetFont( { size: Math.round( buttonOptions.height * 0.85 ) } ) // Equation empirically determined, makes font smaller for larger tables.
               } ) );
             }
             else {