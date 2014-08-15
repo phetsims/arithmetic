bc1b04561f6efa1393b4eb5050d075e17d1e190a
Sam Reid <reids@colorado.edu>
2014-08-15 14:04:07 -0600

    updated docs, see #56

--- js/common/model/FaceModel.js
+++ js/common/model/FaceModel.js
@@ -14,7 +14,8 @@ define( function( require ) {
 
   function FaceModel() {
     PropertySet.call( this, {
-      scoreFace: 1, // score near smile face
+
+      scoreFace: 1, // score to be displayed near the smile face.  In this simulation, you get 1 point if you get a challenge correct, and zero otherwise.
       isVisible: false, // flag of smile face visibility
       isSmile: true // flag of smile face emotion
     } );
