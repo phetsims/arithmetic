814486df1fb5bf7d459f71d98c599533e1a67a77
Chris Malley <cmalley@pixelzoom.com>
2014-10-20 12:55:38 -0600

    fix comments that referred to LevelStartButton

--- js/common/model/LevelModel.js
+++ js/common/model/LevelModel.js
@@ -23,8 +23,8 @@ define( function( require ) {
       bestTime: null,  // best time for level
 
       // currentScore is property using for storing earned points in level.
-      // displayScore is using for displaying correct star number in LevelStartButton.
-      // When level is finished currentScore is set to 0, but stars in LevelStartButton should be displayed so both properties are necessary.
+      // displayScore is using for displaying correct star number in LevelSelectionButton.
+      // When level is finished currentScore is set to 0, but stars in LevelSelectionButton should be displayed so both properties are necessary.
       currentScore: 0, // current score for level
       displayScore: 0 // score displaying in level select buttons
     } );
