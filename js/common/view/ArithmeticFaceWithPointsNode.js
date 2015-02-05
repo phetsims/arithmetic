876f58f630a2f5b39126e8213e829e87fcd15d51
jbphet <john.blanco@colorado.edu>
2015-02-05 09:08:25 -0700

    Simplified this type to use only a single timer for both the opaque period and the fade period.  This results in less code and fixes #125.

--- js/common/view/ArithmeticFaceWithPointsNode.js
+++ js/common/view/ArithmeticFaceWithPointsNode.js
@@ -1,10 +1,11 @@
 // Copyright 2002-2014, University of Colorado Boulder
 
 /**
- * Node that represents a smiling face with the additional points gained for getting the answer correct shown
- * immediately below it.
+ * Node that represents a smiling or frowning face with the additional points gained for getting the answer correct
+ * shown immediately below it.  When shown, this node appears at full opacity for a while, then fades out.
  *
  * @author Andrey Zelenkov (MLearner)
+ * @author John Blanco
  */
 define( function( require ) {
   'use strict';
@@ -12,13 +13,14 @@ define( function( require ) {
   // modules
   var FaceWithPointsNode = require( 'SCENERY_PHET/FaceWithPointsNode' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var Util = require( 'DOT/Util' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var Timer = require( 'JOIST/Timer' );
 
   // constants
-  var SMILE_OPAQUE_TIME = 1000; // time in milliseconds
-  var SMILE_FADE_TIME = 1000; // time in milliseconds
-  var FADE_STEPS = 25;
+  var UPDATE_PERIOD = 1 / 60 * 1000; // milliseconds, intended to match the expected frame rate
+  var OPAQUE_TIME = 1000; // milliseconds
+  var FADE_TIME = 1000; // milliseconds
 
   /**
    * @param {Object} faceModel model for smile face.
@@ -31,10 +33,8 @@ define( function( require ) {
 
     FaceWithPointsNode.call( this, _.extend( {
       pointsFont: new PhetFont( { size: 26, weight: 'bold' } ),
-      visible: false // Initially invisible, must receive an showFace event to become visible.
+      visible: false // Initially invisible, must receive a showFace event to become visible.
     }, options ) );
-
-    // add observers
 
     // set score of smile face
     faceModel.property( 'pointsToDisplay' ).link( function( points ) {
@@ -51,39 +51,40 @@ define( function( require ) {
       }
     } );
 
-    // Timer IDs for pre-fade and fade timers.
-    var opaqueTimeID = null, fadeTimeID = null;
+    // Timer for fading the face.
+    var timerID = null;
 
+    // Handle the event that indicates that the face should be shown.
     faceModel.on( 'showFace', function() {
 
       // make face fully visible
       self.visible = true;
       self.opacity = 1;
 
-      // set up timers to leave the face as fully opaque for a bit, then fade out
-      opaqueTimeID = Timer.setTimeout( function() {
-        opaqueTimeID = null;
-        fadeTimeID = Timer.setInterval( function() {
-          self.opacity -= 1 / FADE_STEPS;
-          if ( self.opacity <= 0 ) {
-            Timer.clearInterval( fadeTimeID );
-            fadeTimeID = null;
+      // Set the countdown to the total for the opaque time and the fade time.
+      var countdown = OPAQUE_TIME + FADE_TIME;
+
+      // If the timer isn't already running, start it up.
+      if ( timerID === null ) {
+        timerID = Timer.setInterval( function() {
+          countdown -= UPDATE_PERIOD;
+          self.opacity = Util.clamp( countdown / FADE_TIME, 0, 1 );
+          if ( self.opacity === 0 ) {
+            Timer.clearInterval( timerID );
+            timerID = null;
             self.visible = false;
           }
-        }, SMILE_FADE_TIME / FADE_STEPS );
-      }, SMILE_OPAQUE_TIME );
+        }, UPDATE_PERIOD );
+      }
     } );
 
+    // Handle the event that indicates that the face should be hidden.
     faceModel.on( 'hideFace', function() {
 
-      // Cancel the timers used to fade out the face.
-      if ( opaqueTimeID !== null ) {
-        Timer.clearTimeout( opaqueTimeID );
-        opaqueTimeID = null;
-      }
-      if ( fadeTimeID !== null ) {
-        Timer.clearInterval( fadeTimeID );
-        fadeTimeID = null;
+      // Cancel the timer (if running)
+      if ( timerID !== null ) {
+        Timer.clearTimeout( timerID );
+        timerID = null;
       }
 
       // Go completely invisible.
