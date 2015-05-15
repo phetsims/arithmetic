6f8e1713c93f67cac25a525b378e1574744bec40
Chris Malley <cmalley@pixelzoom.com>
2015-05-15 16:51:07 -0600

    move all 3rd-party libs to sherpa/lib/, phetsims/sherpa#17

--- js/arithmetic-config.js
+++ js/arithmetic-config.js
@@ -14,7 +14,7 @@ require.config( {
   paths: {
 
     // third-party libs
-    text: '../../sherpa/text-2.0.12',
+    text: '../../sherpa/lib/text-2.0.12',
 
     // PhET plugins
     audio: '../../chipper/js/requirejs-plugins/audio',
