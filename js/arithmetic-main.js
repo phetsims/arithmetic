7da6fcfa1bb95ab2af7f34391f4d5b9504c85408
samreid <reids@colorado.edu>
2016-08-16 21:01:25 -0600

    Added Tandem.createRootTandem() to Sim.js and removed Sim.options.tandem, see https://github.com/phetsims/phet-io/issues/549

--- js/arithmetic-main.js
+++ js/arithmetic-main.js
@@ -29,8 +29,7 @@ define( function( require ) {
       team: 'Bryce Gruneich, Karina K. R. Hensberry, Trish Loeblein, Ariel Paul, Kathy Perkins, Beth Stade',
       qualityAssurance: 'Steele Dalton, Bryce Griebenow, Elise Morgan, Oliver Orejola, Ben Roberts, Bryan Yoelin',
       thanks: 'Thanks to Mobile Learner Labs for working with the PhET development team to convert this simulation to HTML5.'
-    },
-    tandem: tandem
+    }
   };
 
   SimLauncher.launch( function() {
