61dae3e394b48c05f6b8a45c3164f2456eac4f50
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-18 14:21:45 -0700

    Update copyright year, see https://github.com/phetsims/tasks/issues/988

--- js/common/view/EquationInputNode.js
+++ js/common/view/EquationInputNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2018, University of Colorado Boulder
+// Copyright 2014-2019, University of Colorado Boulder
 
 /**
  * Node that looks like an edit box and that is used to either display a fixed value or to show what the user has
