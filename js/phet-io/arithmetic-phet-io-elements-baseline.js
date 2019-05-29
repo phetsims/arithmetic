7bd4b09912e5cf88e0c4a722d6a8e3a62a42e4b3
zepumph <michael.kauzmann@colorado.edu>
2019-05-29 15:52:10 -0800

    recompute baseline, https://github.com/phetsims/phet-io/issues/1466

--- js/phet-io/arithmetic-phet-io-elements-baseline.js
+++ js/phet-io/arithmetic-phet-io-elements-baseline.js
@@ -2160,7 +2160,7 @@ window.phet.phetio.phetioElementsBaseline =
     "arithmetic.navigationBar.phetButton.pickableProperty": {
       "phetioDocumentation": "Set whether the phetButton will be pickable (and hence interactive), see the NodeIO documentation for more details",
       "phetioEventType": "model",
-      "phetioFeatured": true,
+      "phetioFeatured": false,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
