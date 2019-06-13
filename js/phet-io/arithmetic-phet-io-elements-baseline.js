a92316e1fc59ccd335d20055f357d82d201afae3
zepumph <michael.kauzmann@colorado.edu>
2019-06-13 13:42:56 -0800

    update baselines and remove redundant GQ overrides, https://github.com/phetsims/phet-io/issues/1466

--- js/phet-io/arithmetic-phet-io-elements-baseline.js
+++ js/phet-io/arithmetic-phet-io-elements-baseline.js
@@ -1973,7 +1973,7 @@ window.phet.phetio.phetioElementsBaseline =
     "arithmetic.navigationBar.phetButton.enabledProperty": {
       "phetioDocumentation": "When disabled, the button is grayed out and cannot be pressed",
       "phetioEventType": "model",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
