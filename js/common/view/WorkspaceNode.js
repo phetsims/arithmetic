0873171ec611f8b62909fdb7b287bbd46c7d54ef
phet-dev <phetadmin@gmail.com>
2020-02-06 03:05:20 -0700

    update copyright dates from daily grunt work

--- js/common/view/WorkspaceNode.js
+++ js/common/view/WorkspaceNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2019, University of Colorado Boulder
+// Copyright 2014-2020, University of Colorado Boulder
 
 /**
  * A composite node that contains all game components: times table, keypad, equation, smile face, etc.  Some of these
