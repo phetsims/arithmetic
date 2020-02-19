957f91e07f9d72a612f27d027344e6a88eb655f6
chrisklus <christopher.klusendorf@colorado.edu>
2020-02-19 18:11:15 -0500

    phetioFeature titleTextNode.textProperty, see https://github.com/phetsims/joist/issues/597

--- js/phet-io/arithmetic-phet-io-elements-baseline.js
+++ js/phet-io/arithmetic-phet-io-elements-baseline.js
@@ -2254,7 +2254,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioDocumentation": "Property for the displayed text",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioIsArchetype": false,
       "phetioPlayback": false,
