313effb4d7504f27a20ffcc83353c113c93ef892
chrisklus <christopher.klusendorf@colorado.edu>
2020-03-05 19:45:46 -0500

    Update resizeAction docs, see https://github.com/phetsims/tandem/issues/108

--- js/phet-io/arithmetic-phet-io-elements-baseline.js
+++ js/phet-io/arithmetic-phet-io-elements-baseline.js
@@ -327,7 +327,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PhetioCommandProcessorIO"
     },
     "arithmetic.general.model.resizeAction": {
-      "phetioDocumentation": "Executes when the sim is resized. Values are in CSS pixels. The arguments are:<br/><ol><li>width: NumberIO</li><br/><li>height: NumberIO</li></ol>",
+      "phetioDocumentation": "Executes when the sim is resized. Values are the sim dimensions in CSS pixels. The arguments are:<br/><ol><li>width: NumberIO</li><br/><li>height: NumberIO</li></ol>",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
