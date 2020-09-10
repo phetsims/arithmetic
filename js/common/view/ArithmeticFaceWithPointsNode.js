f94a904859a306431589bbd6fe49c124e6fc411a
pixelzoom <cmalley@pixelzoom.com>
2020-09-10 13:17:46 -0600

    convert FaceWithPointsNode to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/common/view/ArithmeticFaceWithPointsNode.js
+++ js/common/view/ArithmeticFaceWithPointsNode.js
@@ -10,7 +10,6 @@
 
 import timer from '../../../../axon/js/timer.js';
 import Utils from '../../../../dot/js/Utils.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import merge from '../../../../phet-core/js/merge.js';
 import FaceWithPointsNode from '../../../../scenery-phet/js/FaceWithPointsNode.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
@@ -21,80 +20,78 @@ const UPDATE_PERIOD = 1 / 60 * 1000; // milliseconds, intended to match the expe
 const OPAQUE_TIME = 1000; // milliseconds
 const FADE_TIME = 1000; // milliseconds
 
-/**
- * @param {Object} faceModel model for smile face.
- * @param {Object} [options] for face node.
- *
- * @constructor
- */
-function ArithmeticFaceWithPointsNode( faceModel, options ) {
-  const self = this;
+class ArithmeticFaceWithPointsNode extends FaceWithPointsNode {
 
-  FaceWithPointsNode.call( this, merge( {
-    pointsFont: new PhetFont( { size: 26, weight: 'bold' } ),
-    visible: false // Initially invisible, must receive a showFace event to become visible.
-  }, options ) );
+  /**
+   * @param {Object} faceModel model for smile face.
+   * @param {Object} [options] for face node.
+   */
+  constructor( faceModel, options ) {
 
-  // set score of smile face
-  faceModel.pointsToDisplayProperty.link( function( points ) {
-    self.setPoints( points );
-  } );
+    options = merge( {
+      pointsFont: new PhetFont( { size: 26, weight: 'bold' } ),
+      visible: false // Initially invisible, must receive a showFace event to become visible.
+    }, options );
 
-  // set the facial expression
-  faceModel.isSmileProperty.link( function( isFaceSmile ) {
-    if ( isFaceSmile ) {
-      self.smile();
-    }
-    else {
-      self.frown();
-    }
-  } );
+    super( options );
 
-  // Timer for fading the face.
-  let timerID = null;
+    // set score of smile face
+    faceModel.pointsToDisplayProperty.link( points => this.setPoints( points ) );
 
-  // Handle the event that indicates that the face should be shown.
-  faceModel.showFaceEmitter.addListener( function() {
-
-    // make face fully visible
-    self.visible = true;
-    self.opacity = 1;
-
-    // Set the countdown to the total for the opaque time and the fade time.
-    let countdown = OPAQUE_TIME + FADE_TIME;
-
-    // cancel previous timer if it exists
-    if ( timerID !== null ) {
-      timer.clearInterval( timerID );
-    }
-
-    // start up the new timer
-    timerID = timer.setInterval( function() {
-      countdown -= UPDATE_PERIOD;
-      self.opacity = Utils.clamp( countdown / FADE_TIME, 0, 1 );
-      if ( self.opacity === 0 ) {
-        timer.clearInterval( timerID );
-        timerID = null;
-        self.visible = false;
+    // set the facial expression
+    faceModel.isSmileProperty.link( isFaceSmile => {
+      if ( isFaceSmile ) {
+        this.smile();
       }
-    }, UPDATE_PERIOD );
-  } );
+      else {
+        this.frown();
+      }
+    } );
 
-  // Handle the event that indicates that the face should be hidden.
-  faceModel.hideFaceEmitter.addListener( function() {
+    // Timer for fading the face.
+    let timerID = null;
 
-    // Cancel the timer (if running)
-    if ( timerID !== null ) {
-      timer.clearTimeout( timerID );
-      timerID = null;
-    }
+    // Handle the event that indicates that the face should be shown.
+    faceModel.showFaceEmitter.addListener( () => {
 
-    // Go completely invisible.
-    self.visible = false;
-  } );
+      // make face fully visible
+      this.visible = true;
+      this.opacity = 1;
+
+      // Set the countdown to the total for the opaque time and the fade time.
+      let countdown = OPAQUE_TIME + FADE_TIME;
+
+      // cancel previous timer if it exists
+      if ( timerID !== null ) {
+        timer.clearInterval( timerID );
+      }
+
+      // start up the new timer
+      timerID = timer.setInterval( () => {
+        countdown -= UPDATE_PERIOD;
+        this.opacity = Utils.clamp( countdown / FADE_TIME, 0, 1 );
+        if ( this.opacity === 0 ) {
+          timer.clearInterval( timerID );
+          timerID = null;
+          this.visible = false;
+        }
+      }, UPDATE_PERIOD );
+    } );
+
+    // Handle the event that indicates that the face should be hidden.
+    faceModel.hideFaceEmitter.addListener( () => {
+
+      // Cancel the timer (if running)
+      if ( timerID !== null ) {
+        timer.clearTimeout( timerID );
+        timerID = null;
+      }
+
+      // Go completely invisible.
+      this.visible = false;
+    } );
+  }
 }
 
 arithmetic.register( 'ArithmeticFaceWithPointsNode', ArithmeticFaceWithPointsNode );
-
-inherit( FaceWithPointsNode, ArithmeticFaceWithPointsNode );
 export default ArithmeticFaceWithPointsNode;
