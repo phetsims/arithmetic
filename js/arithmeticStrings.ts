28a4d569a2f5c3d8c31bd1362c97e0fc00389f7b
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:06 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/arithmeticStrings.ts
@@ -0,0 +1,32 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import arithmetic from './arithmetic.js';
+
+type StringsType = {
+  'arithmetic': {
+    'title': string
+  },
+  'check': string,
+  'chooseYourLevel': string,
+  'divide': string,
+  'factor': string,
+  'multiply': string,
+  'pattern': {
+    'level': {
+      '0levelNumber': string
+    }
+  },
+  'tryAgain': string,
+  'unknownValueIndicator': string
+};
+
+const arithmeticStrings = getStringModule( 'ARITHMETIC' ) as StringsType;
+
+arithmetic.register( 'arithmeticStrings', arithmeticStrings );
+
+export default arithmeticStrings;
