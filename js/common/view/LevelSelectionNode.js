efd2a27ddc5a6b90731aed397604d8415842d01a
phet-dev <phetadmin@gmail.com>
2022-06-21 03:25:12 -0600

    update copyright dates from daily grunt work

--- js/common/view/LevelSelectionNode.js
+++ js/common/view/LevelSelectionNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
 /**
  * Screen that allows the user to select the game level that they wish to play.
