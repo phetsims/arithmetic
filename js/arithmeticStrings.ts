82c6660e7595580fa8ace8ab45c6d0a2be541762
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:41 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/arithmeticStrings.ts
+++ js/arithmeticStrings.ts
@@ -5,24 +5,34 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import arithmetic from './arithmetic.js';
 
 type StringsType = {
   'arithmetic': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'check': string;
+  'checkProperty': TReadOnlyProperty<string>;
   'chooseYourLevel': string;
+  'chooseYourLevelProperty': TReadOnlyProperty<string>;
   'divide': string;
+  'divideProperty': TReadOnlyProperty<string>;
   'factor': string;
+  'factorProperty': TReadOnlyProperty<string>;
   'multiply': string;
+  'multiplyProperty': TReadOnlyProperty<string>;
   'pattern': {
     'level': {
       '0levelNumber': string;
+      '0levelNumberProperty': TReadOnlyProperty<string>;
     }
   };
   'tryAgain': string;
+  'tryAgainProperty': TReadOnlyProperty<string>;
   'unknownValueIndicator': string;
+  'unknownValueIndicatorProperty': TReadOnlyProperty<string>;
 };
 
 const arithmeticStrings = getStringModule( 'ARITHMETIC' ) as StringsType;
