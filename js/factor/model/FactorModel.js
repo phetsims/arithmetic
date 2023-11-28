b5c7020c5225747a6d86f8071243fb3ee2d33687
phet-dev <phetadmin@gmail.com>
2023-11-28 03:10:55 -0700

    update copyright dates from daily grunt work

--- js/factor/model/FactorModel.js
+++ js/factor/model/FactorModel.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * Model for factor game in 'Arithmetic' simulation.
--- js/multiply/MultiplyScreen.js
+++ js/multiply/MultiplyScreen.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * The 'Multiply' screen. Conforms to the contract specified in joist/Screen.
