4af7d1dfe6c60eab9c7ebbc975288e41b0e7414c
Marla Schulz <marlaa.schulz@gmail.com>
2023-12-05 10:49:28 -0700

    Add in licenses, see: https://github.com/phetsims/arithmetic/issues/198

--- js/common/view/BoxPlayerCharacterSetUSA.js
+++ js/common/view/BoxPlayerCharacterSetUSA.js
@@ -9,15 +9,15 @@
 
 import JoistStrings from '../../../../joist/js/JoistStrings.js';
 import { USA_REGION_AND_CULTURE_ID } from '../../../../joist/js/preferences/RegionAndCulturePortrayal.js';
-import divideLevel1Icon_png from '../../../mipmaps/divideLevel1Icon_png.js';
-import divideLevel2Icon_png from '../../../mipmaps/divideLevel2Icon_png.js';
-import divideLevel3Icon_png from '../../../mipmaps/divideLevel3Icon_png.js';
-import factorLevel1Icon_png from '../../../mipmaps/factorLevel1Icon_png.js';
-import factorLevel2Icon_png from '../../../mipmaps/factorLevel2Icon_png.js';
-import factorLevel3Icon_png from '../../../mipmaps/factorLevel3Icon_png.js';
-import multiplyLevel1Icon_png from '../../../mipmaps/multiplyLevel1Icon_png.js';
-import multiplyLevel2Icon_png from '../../../mipmaps/multiplyLevel2Icon_png.js';
-import multiplyLevel3Icon_png from '../../../mipmaps/multiplyLevel3Icon_png.js';
+import divideLevel1Icon_png from '../../../mipmaps/usa/divideLevel1Icon_png.js';
+import divideLevel2Icon_png from '../../../mipmaps/usa/divideLevel2Icon_png.js';
+import divideLevel3Icon_png from '../../../mipmaps/usa/divideLevel3Icon_png.js';
+import factorLevel1Icon_png from '../../../mipmaps/usa/factorLevel1Icon_png.js';
+import factorLevel2Icon_png from '../../../mipmaps/usa/factorLevel2Icon_png.js';
+import factorLevel3Icon_png from '../../../mipmaps/usa/factorLevel3Icon_png.js';
+import multiplyLevel1Icon_png from '../../../mipmaps/usa/multiplyLevel1Icon_png.js';
+import multiplyLevel2Icon_png from '../../../mipmaps/usa/multiplyLevel2Icon_png.js';
+import multiplyLevel3Icon_png from '../../../mipmaps/usa/multiplyLevel3Icon_png.js';
 import BoxPlayerCharacterSet from './BoxPlayerCharacterSet.js';
 
 const ExplorerCharacterSetUSA = new BoxPlayerCharacterSet(
