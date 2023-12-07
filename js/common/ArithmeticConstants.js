2f7bcf8091368a19bbf74a9cc97c74f3eb842a17
phet-dev <phetadmin@gmail.com>
2023-12-07 03:11:22 -0700

    update copyright dates from daily grunt work

--- js/common/ArithmeticConstants.js
+++ js/common/ArithmeticConstants.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * Constants used in multiple locations within the 'Arithmetic' simulation.
