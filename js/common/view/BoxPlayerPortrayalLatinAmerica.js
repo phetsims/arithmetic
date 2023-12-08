81b4137f9da141c31e4cfe8c160c7d4331190bb5
Marla Schulz <marlaa.schulz@gmail.com>
2023-12-07 17:41:51 -0700

    Switch pngs to svgs, see: https://github.com/phetsims/scenery/issues/1578

--- js/common/view/BoxPlayerPortrayalLatinAmerica.js
+++ js/common/view/BoxPlayerPortrayalLatinAmerica.js
@@ -9,28 +9,28 @@
 
 import JoistStrings from '../../../../joist/js/JoistStrings.js';
 import { LATIN_AMERICA_REGION_AND_CULTURE_ID } from '../../../../joist/js/preferences/RegionAndCulturePortrayal.js';
-import divideLevel1Icon_png from '../../../mipmaps/latin-america/divideLevel1Icon_png.js';
-import divideLevel2Icon_png from '../../../mipmaps/latin-america/divideLevel2Icon_png.js';
-import divideLevel3Icon_png from '../../../mipmaps/latin-america/divideLevel3Icon_png.js';
-import factorLevel1Icon_png from '../../../mipmaps/latin-america/factorLevel1Icon_png.js';
-import factorLevel2Icon_png from '../../../mipmaps/latin-america/factorLevel2Icon_png.js';
-import factorLevel3Icon_png from '../../../mipmaps/latin-america/factorLevel3Icon_png.js';
-import multiplyLevel1Icon_png from '../../../mipmaps/latin-america/multiplyLevel1Icon_png.js';
-import multiplyLevel2Icon_png from '../../../mipmaps/latin-america/multiplyLevel2Icon_png.js';
-import multiplyLevel3Icon_png from '../../../mipmaps/latin-america/multiplyLevel3Icon_png.js';
+import divideLevel1Icon_svg from '../../../images/latin-america/divideLevel1Icon_svg.js';
+import divideLevel2Icon_svg from '../../../images/latin-america/divideLevel2Icon_svg.js';
+import divideLevel3Icon_svg from '../../../images/latin-america/divideLevel3Icon_svg.js';
+import factorLevel1Icon_svg from '../../../images/latin-america/factorLevel1Icon_svg.js';
+import factorLevel2Icon_svg from '../../../images/latin-america/factorLevel2Icon_svg.js';
+import factorLevel3Icon_svg from '../../../images/latin-america/factorLevel3Icon_svg.js';
+import multiplyLevel1Icon_svg from '../../../images/latin-america/multiplyLevel1Icon_svg.js';
+import multiplyLevel2Icon_svg from '../../../images/latin-america/multiplyLevel2Icon_svg.js';
+import multiplyLevel3Icon_svg from '../../../images/latin-america/multiplyLevel3Icon_svg.js';
 import BoxPlayerPortrayal from './BoxPlayerPortrayal.js';
 
 const BoxPlayerPortrayalLatinAmerica = new BoxPlayerPortrayal(
   JoistStrings.preferences.tabs.localization.regionAndCulture.portrayalSets.latinAmericaStringProperty,
-  multiplyLevel1Icon_png,
-  multiplyLevel2Icon_png,
-  multiplyLevel3Icon_png,
-  factorLevel1Icon_png,
-  factorLevel2Icon_png,
-  factorLevel3Icon_png,
-  divideLevel1Icon_png,
-  divideLevel2Icon_png,
-  divideLevel3Icon_png,
+  multiplyLevel1Icon_svg,
+  multiplyLevel2Icon_svg,
+  multiplyLevel3Icon_svg,
+  factorLevel1Icon_svg,
+  factorLevel2Icon_svg,
+  factorLevel3Icon_svg,
+  divideLevel1Icon_svg,
+  divideLevel2Icon_svg,
+  divideLevel3Icon_svg,
   LATIN_AMERICA_REGION_AND_CULTURE_ID
 );
 
