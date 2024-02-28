f79572ddcf8a5e495df38d2f8370b1899e6eedb4
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2024-02-28 12:01:35 -0700

    Update credits. See https://github.com/phetsims/arithmetic/issues/212.

--- js/arithmetic-main.js
+++ js/arithmetic-main.js
@@ -32,9 +32,9 @@ const simOptions = {
   preferencesModel: preferencesModel,
   credits: {
     leadDesign: 'Michael Dubson, Amanda McGarry',
-    softwareDevelopment: 'John Blanco, Michael Dubson',
+    softwareDevelopment: 'John Blanco, Michael Dubson, Luisa Vargas',
     team: 'Bryce Gruneich, Karina K. R. Hensberry, Trish Loeblein, Ariel Paul, Kathy Perkins, Beth Stade',
-    qualityAssurance: 'Steele Dalton, Bryce Griebenow, Elise Morgan, Oliver Orejola, Ben Roberts, Bryan Yoelin',
+    qualityAssurance: 'Steele Dalton, Bryce Griebenow, Elise Morgan, Oliver Orejola, Ben Roberts, Bryan Yoelin, Clifford Hardin, Kathryn Woessner, Nancy Salpepi',
     thanks: 'Thanks to Mobile Learner Labs for working with the PhET development team to convert this simulation to HTML5.'
   }
 };
