8d41390f23907fb3ae4b5c79265969aeacfaa1e1
phet-dev <phetadmin@gmail.com>
2024-03-09 03:12:31 -0700

    update copyright dates from daily grunt work

--- js/common/view/BoxPlayerImages.js
+++ js/common/view/BoxPlayerImages.js
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 /**
  * BoxPlayer images contains an array of portrayals, each representing a different region/culture.
  *
