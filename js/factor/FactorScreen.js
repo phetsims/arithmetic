ac8672567f96b284b2e580a221900cd1384c559b
phet-dev <phetadmin@gmail.com>
2024-03-13 03:12:17 -0600

    update copyright dates from daily grunt work

--- js/factor/FactorScreen.js
+++ js/factor/FactorScreen.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2023, University of Colorado Boulder
+// Copyright 2014-2024, University of Colorado Boulder
 
 /**
  * The 'Factor' screen. Conforms to the contract specified in joist/Screen.
